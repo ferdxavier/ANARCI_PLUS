7050b7e44a90573eab386a63595a1b5c5d768124
Fernando Xavier <ferdxavier@gmail.com>
2024-12-12 03:16:38 -0300

    Update sequences and 4_script_install_py_anarci.sh

--- 4_script_install_py_anarci.sh
+++ 4_script_install_py_anarci.sh
@@ -5,11 +5,11 @@ HMMER_VENV_VERSION=0.2.4
 
 python$PYTHON_ENV_VERSION -m venv /workdir/_venv &&
 source /workdir/_venv/bin/activate &&
-cd /workdir/ANARCI &&
-pip uninstall setuptools &&
-pip install setuptools==$SETUPTOOLS_VERSION &&
-pip install biopython==$BIOPYTHON_VERSION &&
-pip install hmmer==$HMMER_VENV_VERSION
+#cd /workdir/ANARCI &&
+#pip uninstall setuptools &&
+#pip install setuptools==$SETUPTOOLS_VERSION &&
+#pip install biopython==$BIOPYTHON_VERSION &&
+#pip install hmmer==$HMMER_VENV_VERSION
 
 . /usr/spack/share/spack/setup-env.sh && 
 spack env create ab && 
