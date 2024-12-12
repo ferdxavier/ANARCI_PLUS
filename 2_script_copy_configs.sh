f8c109cdc7945a136bf9b5b117c5ffeae64a1d47
Fernando Xavier <ferdxavier@gmail.com>
2024-12-12 03:21:03 -0300

    Add packege.py inside ANARCI

--- 2_script_copy_configs.sh
+++ 2_script_copy_configs.sh
@@ -1,2 +1,3 @@
 cp -r ./config/setup.py ./ANARCI/ && 
-cp -r ./config/RUN_pipeline.sh ./ANARCI/build_pipeline/
+cp -r ./config/RUN_pipeline.sh ./ANARCI/build_pipeline/ &&
+cp -r ./config/package.py ./ANARCI/
