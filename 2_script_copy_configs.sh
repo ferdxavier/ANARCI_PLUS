46b2654775393bb9c0875cbd70729c104d21df22
Fernando Xavier <ferdxavier@gmail.com>
2024-12-12 10:47:17 -0300

    Apos backup spack

--- 2_script_copy_configs.sh
+++ 2_script_copy_configs.sh
@@ -2,7 +2,7 @@ cp -r ./config/setup.py ./ANARCI/ &&
 cp -r ./config/RUN_pipeline.sh ./ANARCI/build_pipeline/ &&
 cp -r ./config/package.py ./ANARCI/
 
-rm -rf ANARCI/.git
-git -C ANARCI/ init
+#rm -rf ANARCI/.git
+#git -C ANARCI/ init
 git -C ANARCI/ add .
 git -C ANARCI/ commit -m "Create new ANARCI repo"
